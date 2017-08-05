04bf854cbf2b09df57541764d5803ff8297ed2df
Matt McFadden <themcfadden@gmail.com>
2017-08-05 07:53:42 -0700

    Working on serial and UI

--- server/server_ssl.js
+++ server/server_ssl.js
@@ -67,17 +67,60 @@ var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
 
 var comPort = "COM25";
 var botSerialPort;
+var prevCameraX = 0;
+var prevCameraY = 0;
+var prevMoveX = 0;
+var prevMoveY = 0;
 
+function serialWriteError(err) {
+    if (err) {
+        console.log('Error on SerialPort Write: ', err.message);
+    }
+}
+
+// Pololu protocol: 0xAA, device number, 0x04, channel number,
+//                  target low bits, target high bits
+//                  0xAA 0x0C 0x04 0x00 0x?? 0x??
+//var serialCommand = Buffer.from([0xAA, 0x0C, 0x04, 0x00, 0x00, 0x00]);
+
+// Compact Protocol: 0x84: Set Target
+//                   0xXX: Channel Number
+//                   0xXX: 
+var serialCommand = Buffer.from([0x84, 0x00, 0x00, 0x00]);
+
+var multiplier = 10;
 
 var onEasyrtcMsg = function(connectionObj, msg, socketCallback, next){
     switch(msg.msgType) {
     case 'js1':
-        console.log('Camera:', msg.msgData);
+        if ( ( msg.msgData.x != prevCameraX ) || (msg.msgData.y != prevCameraY))
+        {
+            serialCommand[1] = 0;
+            serialCommand[2] = ((prevCameraX + msg.msgData.x) * multiplier)& 0x7F;
+            serialCommand[3] = (((prevCameraX + msg.msgData.x) * multiplier) >> 7) & 0x7F;
+            botSerialPort.write(serialCommand, serialWriteError);
+
+            console.log('Camera:', msg.msgData, msg.msgData.x,serialCommand[2], serialCommand[3]);
+
+            serialCommand[1] = 1;
+            serialCommand[2] = (msg.msgData.y * multiplier) & 0x7F;
+            serialCommand[3] = ((msg.msgData.y * multiplier) >> 7) & 0x7F;
+            botSerialPort.write(serialCommand, serialWriteError);
+
+            prevCameraX = msg.msgData.x;
+            prevCameraY = msg.msgData.y;
+
+        }
         socketCallback({msgType:'js1', msgData:'done1'});
         next(null);
         break;
     case 'js2':
-        console.log('Move:', msg.msgData);
+        if ( ( msg.msgData.x != prevMoveX ) || (msg.msgData.y != prevMoveY))
+        {
+            console.log('Move:', msg.msgData);
+            prevMoveX = msg.msgData.x;
+            prevMoveY = msg.msgData.y;
+        }
         socketCallback({msgType:'js2', msgData:'done2'});
         next(null);
         break;
@@ -92,6 +135,15 @@ var onEasyrtcMsg = function(connectionObj, msg, socketCallback, next){
                                        });
         console.log('botSerialPort:', botSerialPort);
                                            
+        botSerialPort.on('open', function() {
+            console.log("Serail Port Open");
+        });
+
+        botSerialPort.on('error', function(err) {
+            console.log('Serila Port Error:', err.message);
+        });
+
+
         console.log('Got a callAccepted', msg.msgData);
         if( msg.msgData.who == 'remote')
             console.log('Remote (the Robot) Connected', msg.msgData);
@@ -113,8 +165,6 @@ function successCB(msgType, msgDate) {
 function failCB(msgType, msgDate) {
     console.log("MattMc-- Got fail msg" + msgType + msgData);
 }
-
-
 
 webServer.listen(8433, function () {
     console.log('listening on http://localhost:8443');
